dcfd6cf4b52d358a3287839b87077327b8b52fa5
singrum <miamiq0000@gmail.com>
2026-03-28 17:19:47 +0900

    leftbar ui 버그 수정

--- app/components/graph-data/graph-data.tsx
+++ app/components/graph-data/graph-data.tsx
@@ -1,35 +1,33 @@
-import { useBoundStore } from "@/stores/use-bound-store";
-import { useShallow } from "zustand/react/shallow";
-import { ScrollArea } from "../ui/scroll-area";
-import { Tabs, TabsContent } from "../ui/tabs";
-import GraphDataMenuList, { graphDataMenus } from "./graph-data-menu-list";
+import { useBoundStore } from "@/stores/use-bound-store"
+import { useShallow } from "zustand/react/shallow"
+import { Tabs, TabsContent } from "../ui/tabs"
+import GraphDataMenuList, { graphDataMenus } from "./graph-data-menu-list"
 
 export default function GraphData() {
   const [dataMenu, setDataMenu] = useBoundStore(
-    useShallow((state) => [state.graphDataMenu, state.setGraphDataMenu]),
-  );
+    useShallow((state) => [state.graphDataMenu, state.setGraphDataMenu])
+  )
 
   return (
-    <ScrollArea className="[&_[data-slot=scroll-area-viewport]>div]:block! h-full">
+    <div className="h-full flex-1 overflow-y-scroll">
       <Tabs
         value={dataMenu.toString()}
         onValueChange={(e) => {
-          setDataMenu(Number(e));
+          setDataMenu(Number(e))
         }}
-        className="gap-4 h-full p-4 w-full"
+        className="h-full w-full gap-4 p-4"
       >
         <GraphDataMenuList />
         {graphDataMenus.map((e, index) => (
           <TabsContent
             key={e.value}
             value={index.toString()}
-            forceMount
             hidden={dataMenu !== index}
           >
             <e.component />
           </TabsContent>
         ))}
       </Tabs>
-    </ScrollArea>
-  );
+    </div>
+  )
 }
--- app/components/leftbar/leftbar.tsx
+++ app/components/leftbar/leftbar.tsx
@@ -5,12 +5,12 @@ export default function Leftbar({ children }: { children: React.ReactNode }) {
   const openLeftbar = useBoundStore((state) => state.openLeftbar)
   return (
     <div
-      className={cn("fixed top-0 left-0 h-full max-h-dvh w-75", {
+      className={cn("fixed top-0 left-0 flex h-full min-h-0 w-75 flex-col", {
         "h-auto w-auto p-2": !openLeftbar,
       })}
     >
       <div
-        className={cn("flex h-full flex-col bg-sidebar", {
+        className={cn("flex h-full flex-1 flex-col bg-sidebar", {
           "rounded-md border": !openLeftbar,
         })}
       >
